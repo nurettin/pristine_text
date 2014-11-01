0336561d35cf5593fcb4b01e939c75bca28ff497
Nurettin Onur TUĞCU <onurtugcu@gmail.com>
2014-11-01 08:27:26 +0200

    remove minitest gem dependency

--- test/pristine_text.rb
+++ test/pristine_text.rb
@@ -1,7 +1,7 @@
 require "pristine_text"
 require "minitest/autorun"
 
-class PristineTextTest< Minitest::Test
+class PristineTextTest< Minitest::Unit::TestCase
   def test_clean
     assert_equal PristineText.clean("haberler geliyorlar gidiyorlar", :tr), "haber geliyor gidiyor"
   end
